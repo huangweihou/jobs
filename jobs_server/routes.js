2abcf0ce16e3d65314258552d2f74c4ce83626a7
Wei-Chen Lin <70911832+lin-weichen@users.noreply.github.com>
2020-12-09 10:17:24 +0800

    Update routes.js

--- jobs_server/routes.js
+++ jobs_server/routes.js
@@ -6,11 +6,11 @@ var connection = mysql.createPool(config);
 
 function getJobs(req, res) {
   var jobTitle = req.query.jobTitle;
-  var query = '
+  var query = `
     SELECT *
     FROM DICE d 
     WHERE JOB_TITLE = '${jobTitle}';
-  ';
+  `;
   connection.query(query, function(err, jobTitle, fields){
     if (err) console.log(err);
     else{
@@ -20,17 +20,17 @@ function getJobs(req, res) {
   });
 };
   
-  var location = req.query.location;
-  var jobType = req.query.jobType;
-  var salaryMin = req.query.salaryMin;
-  var salaryMax = req.query.salaryMax;
-  //console.log(jobTitle);
-  console.log(location);
-  console.log(jobType);
-  console.log(salaryMin);
-  console.log(salaryMax);
-  res.send("get");
-}
+//   var location = req.query.location;
+//   var jobType = req.query.jobType;
+//   var salaryMin = req.query.salaryMin;
+//   var salaryMax = req.query.salaryMax;
+//   //console.log(jobTitle);
+//   console.log(location);
+//   console.log(jobType);
+//   console.log(salaryMin);
+//   console.log(salaryMax);
+//   res.send("get");
+// }
 // The exported functions, which can be accessed in index.js.
 module.exports = {
   getJobs: getJobs,
