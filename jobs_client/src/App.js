62cef1f35bc7a37e7aa5c093e5c6c099c39d5508
Yun Ding <71709525+yun-ding@users.noreply.github.com>
2020-12-13 04:03:39 +0800

    App.js
    new page created: /popular & /popular:skill

--- jobs_client/src/App.js
+++ jobs_client/src/App.js
@@ -1,12 +1,35 @@
 import React, { useState } from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom';
 import Jobs from "./pages/job/Jobs";
+import Popular from "./pages/job/Popular";
 
 const App = () => {
   return (
     <div>
-      <Jobs />
+      <Router>
+					<Switch>
+						<Route
+							exact
+							path="/jobs"
+							render={() => (
+								<Jobs />
+							)}
+						/>
+						<Route
+							path="/popular"
+							render={() => (
+								<Popular />
+							)}
+						/>
+					</Switch>
+				</Router>
     </div>
   );
 };
+
 
 export default App;
