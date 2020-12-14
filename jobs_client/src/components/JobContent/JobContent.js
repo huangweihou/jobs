f22679fc4c282ebf737b5c6bb5f75b9c3caf4383
HuangWeiHou <huangwh@seas.upenn.edu>
2020-12-14 14:52:15 +0800

    job content default value

--- jobs_client/src/components/JobContent/JobContent.js
+++ jobs_client/src/components/JobContent/JobContent.js
@@ -5,12 +5,23 @@ const JobContent = (props) => {
   const { selectedJob } = props;
   return (
     <div className="job-detail-container">
-      <h1>JobTitle: {selectedJob ? selectedJob["JobTitle"] : null}</h1>
-      <h2>Location: {selectedJob ? selectedJob["Location"] : null}</h2>
-      <h2>Employer: {selectedJob ? selectedJob["Employer"] : null}</h2>
-      <h2>Job Type: {selectedJob ? selectedJob["JobType"] : null}</h2>
-      <h2>Skill: {selectedJob ? selectedJob["Skill"] : null}</h2>
-      <h2>Description: </h2>
+      {selectedJob ? <h2>JobTitle:</h2> : null}
+      <h3 className="content">
+        {selectedJob ? selectedJob["JobTitle"] : null}
+      </h3>
+      {selectedJob ? <h2>Location:</h2> : null}
+      <h3 className="content">
+        {selectedJob ? selectedJob["Location"] : null}
+      </h3>
+      {selectedJob ? <h2>Employer:</h2> : null}
+      <h3 className="content">
+        {selectedJob ? selectedJob["Employer"] : null}
+      </h3>
+      {selectedJob ? <h2>Job Type:</h2> : null}
+      <h3 className="content">{selectedJob ? selectedJob["JobType"] : null}</h3>
+      {selectedJob ? <h2>Skills:</h2> : null}
+      <h3 className="content">{selectedJob ? selectedJob["Skill"] : null}</h3>
+      {selectedJob ? <h2>Description:</h2> : null}
       <p>{selectedJob ? selectedJob["Description"] : null}</p>
     </div>
   );
